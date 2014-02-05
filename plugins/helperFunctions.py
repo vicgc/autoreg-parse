bb781d74301c88575674506a7b1526824972303a
Patrick Olsen <patrickolsen@sysforensics.org>
2014-02-05 16:12:33 +0800

    Added getComputerName function.

--- plugins/helperFunctions.py
+++ plugins/helperFunctions.py
@@ -1,7 +1,7 @@
-import sys
-import json
 from collections import OrderedDict
 from Registry import Registry
+import sys
+import json
 
 def jsonOutput(header, \
 			   key_item, \
@@ -10,7 +10,8 @@ def jsonOutput(header, \
 			   value_item3, \
 			   value_item4, \
 			   value_item5, \
-			   lastwrite_time):
+			   lastwrite_time,\
+               sys_name):
     d = OrderedDict()
     d['Title']   = header
     d['Name']    = key_item
@@ -20,6 +21,7 @@ def jsonOutput(header, \
     d['Value4']  = value_item4
     d['Value5']  = value_item5
     d['LWTime']  = str(lastwrite_time)
+    d['CompName'] = sys_name
     return d
 
 def getControlSet(reg_sys):
@@ -34,3 +36,12 @@ def getControlSet(reg_sys):
 
 def outputRender(objects_list):
 	sys.stdout.write(json.dumps(objects_list, indent=4))
+
+def getComputerName(reg_sys):
+    
+    current = getControlSet(reg_sys)
+    computerName = reg_sys.open("%s\\Control\\ComputerName\\ComputerName" % (current))
+    for v in computerName.values():
+        if v.name() == "ComputerName":
+            comp_name = v.value()
+        return comp_name
