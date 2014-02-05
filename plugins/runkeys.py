b52ebc5b468e7fae4683455558d3916d4b68faf0
Patrick Olsen <patrickolsen@sysforensics.org>
2014-02-05 11:17:31 +0800

    Updated RunKeys with JSON Output
    Also, re-wrote the parsing code.

--- plugins/runkeys.py
+++ plugins/runkeys.py
@@ -1,43 +1,53 @@
 from Registry import Registry
-from helperFunctions import jsonOutput, outputRender, getControlSet
+from helperFunctions import jsonOutput, outputRender
+
+objects_list = []
 
 def getPlugin(reg_soft, reg_nt, reg_sys):
 
-    print ("\n" + ("=" * 51) + "\nTRADITIONAL \"RUN\" KEYS\n" + ("=" * 51))
+    header = "TRADITIONAL \"RUN\" KEYS"
 
-    hklm_run_list = ["Microsoft\\Windows\\CurrentVersion\\Run",
+    reg_hives = [reg_sys, reg_soft, reg_sys]
+    
+    run_entries =   ["Microsoft\\Windows\\CurrentVersion\\Run",
                      "Microsoft\\Windows\\CurrentVersion\\RunOnce",
                      "Microsoft\\Windows\\CurrentVersion\\RunOnceEx",
                      "Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run",
                      "Microsoft\\Windows\\CurrentVersion\\RunServicesOnce"
                      "Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Run",
-                     "Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\RunOnce"]
+                     "Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
+                     "Software\\Microsoft\\Windows\\CurrentVersion\\Run",
+                     "Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
+                     "Software\\Microsoft\\Windows\\CurrentVersion\\RunServices",
+                     "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run"]
+    
+    for k in run_entries:
+        for hive in reg_hives:
+            try:
+                key = hive.open(k)
+                for v in key.values():
+                    try:
+                        key_name = v.name()
+                    except:
+                        key_name = "???"
+                    try:
+                        key_value = v.value()
+                    except:
+                        key_value = "???"
+                    try:
+                        last_write = str(key.timestamp())
+                    except:
+                        last_write = "???"
 
-    ntuser_run_list = ["Software\\Microsoft\\Windows\\CurrentVersion\\Run",
-                       "Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce",
-                       "Software\\Microsoft\\Windows\\CurrentVersion\\RunServices",
-                       "Software\\Microsoft\\Windows\\CurrentVersion\\Policies\\Explorer\\Run"]
+                    objects_list.append(jsonOutput(header, \
+                                            key_item = key_name, \
+                                            value_item1 = key_value, \
+                                            value_item2 = "???", \
+                                            value_item3 = "???", \
+                                            value_item4 = "???", \
+                                            value_item5 = "???", \
+                                            lastwrite_time = last_write))
+            except Registry.RegistryKeyNotFoundException as e:
+                pass
 
-    try:
-
-        for k in hklm_run_list:
-            key = reg_soft.open(k)
-            for v in key.values():
-                if v.value_type() == Registry.RegSZ or v.value_type() == Registry.RegExpandSZ or v.value_type() == Registry.RegMultiSZ:
-                    print 'Key: %s\nValue: %s\nRegPath: %s\n' % (v.name().encode('ascii', 'ignore'), v.value().encode('ascii', 'ignore'), k)
-
-        for k in hklm_run_list:
-            print k
-            key = reg_sys.open(k)
-            for v in key.values():
-                if v.value_type() == Registry.RegSZ or v.value_type() == Registry.RegExpandSZ or v.value_type() == Registry.RegMultiSZ:
-                    print 'Key: %s\nValue: %s\nRegPath: %s\n' % (v.name().encode('ascii', 'ignore'), v.value().encode('ascii', 'ignore'), k)
-
-        for k in ntuser_run_list:
-            key = reg_nt.open(k)
-            for v in key.values():
-                if v.value_type() == Registry.RegSZ or v.value_type() == Registry.RegExpandSZ or v.value_type() == Registry.RegMultiSZ:
-                    print 'Key: %s\nValue: %s\nRegPath: %s\n' % (v.name().encode('ascii', 'ignore'), v.value().encode('ascii', 'ignore'), k)
-
-    except Registry.RegistryKeyNotFoundException as e:
-        pass
+    outputRender(objects_list)
