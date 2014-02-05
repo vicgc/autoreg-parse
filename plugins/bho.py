09009fe8af06956a3ecf758674f413a6a17e95ee
Patrick Olsen <patrickolsen@sysforensics.org>
2014-02-05 10:46:22 +0800

    Updated with JSON output

--- plugins/bho.py
+++ plugins/bho.py
@@ -1,13 +1,15 @@
 from Registry import Registry
+from collections import OrderedDict
+from helperFunctions import jsonOutput, outputRender
+
+objects_list = []
+BHO_list = []
+header = "Browser Helper Object"
+
+bho_keys = ["Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects",
+            "WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects"]
 
 def getPlugin(reg_soft, reg_nt='', reg_sys=''):
-
-    bho_keys = ["Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects",
-                "WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Browser Helper Objects"]
-
-    BHO_list = []
-
-    print ("\n" + ("=" * 51) + "\nBROWSER HELPER OBJECTS\n" + ("=" * 51))
 
     for b in bho_keys:
         try:
@@ -20,8 +22,29 @@ def getPlugin(reg_soft, reg_nt='', reg_sys=''):
     
     for clsids in BHO_list:
         try:
-            ke = reg_soft.open("Classes\\CLSID\\%s" % (clsids))
-            print "KeyName: %s\nPath: %s\nLastWrite: %s\n" % (ke.name(), ke.subkey("InProcServer32").value('').value(), ke.timestamp())
+            k = reg_soft.open("Classes\\CLSID\\%s" % (clsids))
+            try:
+                key_name = k.name()
+            except:
+                key_name = "???"
+            try:
+                key_value = k.subkey("InProcServer32").value('').value()
+            except:
+                key_value = "???"
+            try:
+                last_write = str(k.timestamp())
+            except:
+                last_write = "???"
+
+            objects_list.append(jsonOutput(header, \
+                                            key_item = k.name(), \
+                                            value_item1 = k.subkey("InProcServer32").value('').value(), \
+                                            value_item2 = "???", \
+                                            value_item3 = "???", \
+                                            value_item4 = "???", \
+                                            value_item5 = "???", \
+                                            lastwrite_time = last_write))
         except Registry.RegistryKeyNotFoundException as e:
             pass
 
+    outputRender(objects_list)
